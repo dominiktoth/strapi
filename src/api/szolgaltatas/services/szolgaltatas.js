07e9dc50465a38210a4212d58028080ecc7cf3f4
domatoth <dominik.toth@garanddesign.hu>
2023-12-27 12:05:09 +0100

    services

---
+++ src/api/szolgaltatas/services/szolgaltatas.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * szolgaltatas service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::szolgaltatas.szolgaltatas');
