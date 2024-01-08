04b907602f52524646025dafd2cec533c9bb7bb1
zakarpatrik <patrik.zakar@garanddesign.hu>
2024-01-08 10:07:24 +0100

    company info strapi

---
+++ src/api/cegadatok/controllers/cegadatok.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cegadatok controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cegadatok.cegadatok');
